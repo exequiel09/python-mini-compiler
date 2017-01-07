99d5f66b7238e0fb1d09a1bd72e816dece2c0242
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2017-01-07 22:41:22 +0800

    Create a compiler class to abstract the compilation logic

--- compiler/__init__.py
+++ compiler/__init__.py
@@ -4,4 +4,23 @@
 # Licensed under MIT
 # Version 0.0.0
 
+from compiler.tokenizer import Tokenizer
+from compiler.parser import Parser
+from compiler.transformer import Transformer
+from compiler.code_generator import CodeGenerator
+
+class Compiler(object):
+  """ Class that compiles given code to another language """
+
+  def __init__(self, input_code):
+    self.input_code = input_code
+
+  def compile(self):
+    tknizer = Tokenizer("(add 23 (subtract 4 2))")
+    parser = Parser(tknizer.run())
+    transformer = Transformer(parser.run())
+    code_generator = CodeGenerator(transformer.run())
+
+    return code_generator.run()
+
 
--- run.py
+++ run.py
@@ -6,31 +6,10 @@
 # Licensed under MIT
 # Version 0.0.0
 
-import pprint
-from compiler.tokenizer import Tokenizer
-from compiler.parser import Parser
-from compiler.transformer import Transformer
-from compiler.code_generator import CodeGenerator
+from compiler import Compiler
 
 if __name__ == "__main__":
-  # setup the pretty printer
-  pp = pprint.PrettyPrinter(indent=2)
-
-  tknizer = Tokenizer("(add 23 (subtract 4 2))")
-
-  parser = Parser(tknizer.run())
-  ast = parser.run()
-
-  # print('Original AST:')
-  # pp.pprint(ast)
-
-  transformer = Transformer(ast)
-  new_ast = transformer.run()
-
-  # print('Transformed AST:')
-  # pp.pprint(new_ast)
-
-  code_generator = CodeGenerator(new_ast)
-  print(code_generator.run())
+  compiler = Compiler("(add 23 (subtract 4 2))")
+  print(compiler.compile())
 
 
