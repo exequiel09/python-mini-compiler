740874eb9beb356e8d82b40370174068ab69b471
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2017-01-07 23:09:22 +0800

    Bump version 1.0.0-alpha1

--- compiler/code_generator.py
+++ compiler/code_generator.py
@@ -2,7 +2,7 @@
 #
 # Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
 # Licensed under MIT
-# Version 0.0.0
+# Version 1.0.0-alpha1
 
 class CodeGenerator(object):
   """ Class that generates Python Code based on the transformer-generated AST """
